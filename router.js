2be647350142a16e912e48dd74241c4da88395c8
Michael Frauenholtz <mafrauen@gmail.com>
2012-01-02 16:18:34 -0600

    Updated files to include POST handling

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
-function route(pathname) {
+function route(handle, pathname, response, postData) {
   console.log("About to log a request for " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response, postData);
+  } else {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
+  }
 }
 
-export.route = route;
+exports.route = route;
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var http = require("http");
 var url = require("url");
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " recieved");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Recieved POST data chunk '"+
+        postDataChunk + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
+
   }
 
   http.createServer(onRequest).listen(8888);
